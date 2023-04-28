baec9f03d4858ee3a1e3bae984cf86e7c9009bc8
Harsh Patel <harshkpatel2002@gmail.com>
2023-04-28 11:24:20 -0500

    Added Form Submission

--- home/views.py
+++ home/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
 
 def index(request):
+    print("HelloWorld")
     return render(request, "index.html")
+
+def process_form(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        location = request.POST['location']
+        interest = request.POST['interest']
+        time_of_day = request.POST['timeOfDay']
+        # Send to GPT API
+        return HttpResponse("Responses Submitted")
+    return render(request, 'index.html')
